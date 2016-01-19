b2f79a3aa1eaf9a7da3a759cf6716e4311524891
Pieter Hintjens <ph@imatix.com>
2016-01-19 13:32:07 +0100

    Problem: git repo doesn't contain build scripts
    Solution: add everything that zproject generates

---
+++ include/global_domination.h
@@ -0,0 +1,16 @@
+/*  =========================================================================
+    global domination - Project
+
+    Copyright (c) the Authors
+    =========================================================================
+*/
+
+#ifndef GLOBAL_DOMINATION_H_INCLUDED
+#define GLOBAL_DOMINATION_H_INCLUDED
+
+//  Include the project library file
+#include "gdom_library.h"
+
+//  Add your own public definitions here, if you need them
+
+#endif
